639dfe5e68cb62ea785816094934cbf7d8c43428
KRISHNA RK BRAND <ravikumar6303444@gmail.com>
2025-05-29 22:37:24 +0530

    Update 0_AADI-CHIPS.js

--- Priyansh/commands/0_AADI-CHIPS.js
+++ Priyansh/commands/0_AADI-CHIPS.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("chips")==0 || event.body.indexOf("Chips")==0 || event.body.indexOf("CHIPS ")==0 || event.body.indexOf("Chipas")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐇𝐀𝐀𝐍 𝐊𝐇𝐀𝐍 𝐊 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐜𝐇𝐚𝐔𝐦𝐈𝐧 \n< ────────────────── >",
+                                body: "꧁•🦋✨🍁❉‌‌্᭄...─┼\n𝗢𝗪𝗡𝗘𝗥   ➻  𝐒ɧɣ𝗔ɱ 𝐃i͠Ꮗ𝗔ɳi͠ \n💚⟶‌‌𓆩〭〬༎🐰❉‌‌্᭄...꧂\n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐜𝐇𝐚𝐔𝐦𝐈𝐧 \n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/Pinterest-mini-aesthetics-snack-party.mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
