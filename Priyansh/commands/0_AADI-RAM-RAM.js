7e7f359a7e07ac14c7637dc883d562bbd2015f01
KRISHNA RK BRAND <ravikumar6303444@gmail.com>
2025-05-29 22:50:20 +0530

    Update 0_AADI-RAM-RAM.js

--- Priyansh/commands/0_AADI-RAM-RAM.js
+++ Priyansh/commands/0_AADI-RAM-RAM.js
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("jay shree ram") ||
 react.includes("JAY SHREE RAM")) {
 		var msg = {
-				body: `𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝐀𝐚̃𝐃𝐢   𝐁𝐚𝐁𝐮__🥰\n\n< ────────────────── >\n 𝐌𝐀𝐈𝐍 𝐓𝐎 𝐒𝐇𝐑𝐄𝐄 𝐑𝐀𝐌 𝐉𝐈  𝐊𝐀 𝐃𝐄𝐄𝐖𝐀𝐍𝐀 𝐇𝐔 𝐏𝐘𝐀𝐑 𝐒𝐄 𝐁𝐎𝐋𝐎 𝐉𝐀𝐘 𝐒𝐇𝐑𝐄𝐄 𝐑𝐀𝐌 😍 ❤️`,attachment: fs.createReadStream(__dirname + `/noprefix/#ram_#ramlalaa_#ayodhya_#siyaram_in_2024___Ram_photos,_Shree_ram_photos,_Shri_ram_photo(360P).mp4`)
+				body: `꧁•🦋✨🍁❉‌‌্᭄...─┼\n𝗢𝗪𝗡𝗘𝗥   ➻  𝐒ɧɣ𝗔ɱ 𝐃i͠Ꮗ𝗔ɳi͠ \n💚⟶‌‌𓆩〭〬༎🐰❉‌‌্᭄...꧂\n< ────────────────── >\n 𝐌𝐀𝐈𝐍 𝐓𝐎 𝐒𝐇𝐑𝐄𝐄 𝐑𝐀𝐌 𝐉𝐈  𝐊𝐀 𝐃𝐄𝐄𝐖𝐀𝐍𝐀 𝐇𝐔 𝐏𝐘𝐀𝐑 𝐒𝐄 𝐁𝐎𝐋𝐎 𝐉𝐀𝐘 𝐒𝐇𝐑𝐄𝐄 𝐑𝐀𝐌 😍 ❤️`,attachment: fs.createReadStream(__dirname + `/noprefix/#ram_#ramlalaa_#ayodhya_#siyaram_in_2024___Ram_photos,_Shree_ram_photos,_Shri_ram_photo(360P).mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🥀", event.messageID, (err) => {}, true)
