2ad89316a824e27ef4320718acf933963c9a3fc0
KRISHNA RK BRAND <ravikumar6303444@gmail.com>
2025-05-29 22:54:33 +0530

    Update 0_AADI_GOOD MORNING.js

--- Priyansh/commands/0_AADI_GOOD MORNING.js
+++ Priyansh/commands/0_AADI_GOOD MORNING.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("GM")==0 || event.body.indexOf("Gm")==0 || event.body.indexOf("Good morning")==0 || event.body.indexOf("GOOD MORNING")==0) {
 		var msg = {
-				body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮\n< ────────────────── >\n\n⎯꯭֯🌸⃪ ꯭⃛֯G❍❍𝐃 ɱ❍ɽηIIηG\n< ────────────────── >",
+				body: "꧁•🦋✨🍁❉‌‌্᭄...─┼\n𝗢𝗪𝗡𝗘𝗥   ➻  𝐒ɧɣ𝗔ɱ 𝐃i͠Ꮗ𝗔ɳi͠ \n💚⟶‌‌𓆩〭〬༎🐰❉‌‌্᭄...꧂\n< ────────────────── >\n\n⎯꯭֯🌸⃪ ꯭⃛֯G❍❍𝐃 ɱ❍ɽηIIηG\n< ────────────────── >",
 				attachment: fs.createReadStream(__dirname + `/noprefix/received_1083155713406602.mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
