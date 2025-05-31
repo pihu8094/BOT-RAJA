4ccafaa9c0b5a433c69a87a0c93e9f79b64c2361
KRISHNA RK BRAND <ravikumar6303444@gmail.com>
2025-05-31 10:12:30 +0530

    Update 0_AADI-OWNER.js

--- Priyansh/commands/0_AADI-OWNER.js
+++ Priyansh/commands/0_AADI-OWNER.js
@@ -18,15 +18,15 @@ module.exports = {
     // Array of Imgur URLs
     handleEvent: async function({ api, event, client, Users, __GLOBAL }) {
         const gifs = [
-            "https://i.imgur.com/RFIWtNE.jpeg",
-            "https://i.imgur.com/RFIWtNE.jpeg",
-            "https://i.imgur.com/RFIWtNE.jpeg",
-            "https://i.imgur.com/J10Z1KR.jpeg",
-            "https://i.imgur.com/sg3eO0k.jpeg",
-            "https://i.imgur.com/BNuCzHx.jpeg"
+            "https://i.ibb.co/7xQf9QMZ/fc2e5a08403bdafd2d4ae1258978e6bc-1.jpg",
+            "https://i.ibb.co/N2YsW5BM/IMG-20250531-095235.jpg",
+            "https://i.ibb.co/GQmhBXKZ/IMG-20250531-095251.jpg",
+            "https://i.ibb.co/7xQf9QMZ/fc2e5a08403bdafd2d4ae1258978e6bc-1.jpg",
+            "https://i.ibb.co/N2YsW5BM/IMG-20250531-095235.jpg",
+            "https://i.ibb.co/GQmhBXKZ/IMG-20250531-095251.jpg"
         ];
 
-        const message = "𝐎𝐰𝐧𝐞𝐫 ➻  🥀  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮\n\n● ────────────────── ●\n\n𝐌𝐘 𝐎𝐰𝐧𝐞𝐑 𝐀𝐚𝐝𝐢 𝐁𝐚𝐁𝐮 .... < 𝐄𝐃𝐈𝐓 > .... 𝐘𝐞 𝐁𝐨𝐓 𝐒𝐢𝐫𝐅 𝐎𝐰𝐧𝐞𝐑 𝐊 𝐋𝐢𝐘𝐞 𝐇 .... 𝐌𝐮𝐣𝐇𝐞 𝐀𝐚𝐏 𝐋𝐨𝐆𝐨 𝐊𝐨 𝐇𝐚𝐬𝐚𝐍𝐞 𝐊 𝐋𝐢𝐘𝐞 𝐁𝐚𝐧𝐘𝐚 𝐆𝐲𝐚 𝐇 𝐓𝐨𝐇 𝐇𝐚𝐩𝐩𝐘 𝐑𝐞𝐇𝐚𝐍𝐀 𝐀𝐩𝐩𝐤𝐚 𝐀𝐩𝐩𝐧𝐚 𝐎𝐰𝐧𝐞𝐑 𝐀𝐚𝐝𝐢 𝐁𝐚𝐁𝐮\n\n● ────────────────── ●\n\n\n𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮";
+        const message = "𝐎𝐰𝐧𝐞𝐫 ➻  🥀  𝐊𝐫𝐢𝐬𝐡𝐧𝐚 𝐛𝐚𝐛𝐮\n\n● ────────────────── ●\n\n𝐌𝐘 𝐎𝐰𝐧𝐞𝐑 𝐊𝐫𝐢𝐬𝐡𝐧𝐚 𝐁𝐚𝐁𝐮 .... < 𝐄𝐃𝐈𝐓 > .... 𝐘𝐞 𝐁𝐨𝐓 𝐒𝐢𝐫𝐅 𝐎𝐰𝐧𝐞𝐑 𝐊 𝐋𝐢𝐘𝐞 𝐇 .... 𝐌𝐮𝐣𝐇𝐞 𝐀𝐚𝐏 𝐋𝐨𝐆𝐨 𝐊𝐨 𝐇𝐚𝐬𝐚𝐍𝐞 𝐊 𝐋𝐢𝐘𝐞 𝐁𝐚𝐧𝐘𝐚 𝐆𝐲𝐚 𝐇 𝐓𝐨𝐇 𝐇𝐚𝐩𝐩𝐘 𝐑𝐞𝐇𝐚𝐍𝐀 𝐀𝐩𝐩𝐤𝐚 𝐀𝐩𝐩𝐧𝐚 𝐎𝐰𝐧𝐞𝐑 𝐊𝐫𝐢𝐬𝐡𝐧𝐚 𝐁𝐚𝐁𝐮\n\n● ────────────────── ●\n\n\n𝐎𝐰𝐧𝐞𝐫 ➻  𝐊𝐫𝐢𝐬𝐡𝐧𝐚 𝐛𝐚𝐛𝐮";
         const { threadID, messageID } = event;
         const lowerCaseMessage = event.body.toLowerCase();
 
@@ -42,7 +42,7 @@ module.exports = {
                     attachment: fs.createReadStream(downloadPath)
                 };
                 api.sendMessage(msg, threadID, messageID);
-                api.setMessageReaction("😘", event.messageID, (err) => {}, true);
+                api.setMessageReaction("🫅", event.messageID, (err) => {}, true);
             });
         }
     },
