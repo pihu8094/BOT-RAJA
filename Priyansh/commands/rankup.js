54686cbee76ad5ee7d6758cb1e9baa17b45d7d85
KRISHNA RK BRAND <ravikumar6303444@gmail.com>
2025-06-09 20:49:19 +0530

    Update rankup.js

--- Priyansh/commands/rankup.js
+++ Priyansh/commands/rankup.js
@@ -1,59 +1,59 @@
 module.exports.config = {
-	name: "rankup",
-	version: "7.3.1",
-	hasPermssion: 1,
-	credits: "PREM BABU",
-	description: "Announce rankup for each group, user",
-	commandCategory: "Edit-IMG",
-	dependencies: {
-		"fs-extra": ""
-	},
-	cooldowns: 2,
+        name: "rankup",
+        version: "7.6.8",
+        hasPermssion: 1,
+        credits: "Kreysh",
+        description: "Announce rankup for each group/user @hiramin ko muna to kreysh thanks ",
+        commandCategory: "Edit-IMG",
+        dependencies: {
+                "fs-extra": ""
+        },
+        cooldowns: 2,
 };
 
 module.exports.handleEvent = async function({ api, event, Currencies, Users, getText }) {
-	var {threadID, senderID } = event;
-	const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+        var {threadID, senderID } = event;
+        const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
   const { loadImage, createCanvas } = require("canvas");
   const fs = global.nodemodule["fs-extra"];
   const axios = global.nodemodule["axios"];
   let pathImg = __dirname + "/noprefix/rankup/rankup.png";
   let pathAvt1 = __dirname + "/cache/Avtmot.png";
   var id1 = event.senderID;
-  
 
-	threadID = String(threadID);
-	senderID = String(senderID);
 
-	const thread = global.data.threadData.get(threadID) || {};
+        threadID = String(threadID);
+        senderID = String(senderID);
 
-	let exp = (await Currencies.getData(senderID)).exp;
-	exp = exp += 1;
+        const thread = global.data.threadData.get(threadID) || {};
 
-	if (isNaN(exp)) return;
+        let exp = (await Currencies.getData(senderID)).exp;
+        exp = exp += 1;
 
-	if (typeof thread["rankup"] != "undefined" && thread["rankup"] == false) {
-		await Currencies.setData(senderID, { exp });
-		return;
-	};
+        if (isNaN(exp)) return;
 
-	const curLevel = Math.floor((Math.sqrt(1 + (4 * exp / 3) + 1) / 2));
-	const level = Math.floor((Math.sqrt(1 + (4 * (exp + 1) / 3) + 1) / 2));
+        if (typeof thread["rankup"] != "undefined" && thread["rankup"] == false) {
+                await Currencies.setData(senderID, { exp });
+                return;
+        };
 
-	if (level > curLevel && level != 1) {
-		const name = global.data.userName.get(senderID) || await Users.getNameUser(senderID);
-		var messsage = (typeof thread.customRankup == "undefined") ? msg = getText("levelup") : msg = thread.customRankup, 
-			arrayContent;
+        const curLevel = Math.floor((Math.sqrt(1 + (4 * exp / 3) + 1) / 2));
+        const level = Math.floor((Math.sqrt(1 + (4 * (exp + 1) / 3) + 1) / 2));
 
-		messsage = messsage
-			.replace(/\{name}/g, name)
-			.replace(/\{level}/g, level);
+        if (level > curLevel && level != 1) {
+                const name = global.data.userName.get(senderID) || await Users.getNameUser(senderID);
+                var messsage = (typeof thread.customRankup == "undefined") ? msg = getText("levelup") : msg = thread.customRankup, 
+                        arrayContent;
 
-		const moduleName = this.config.name;
+                messsage = messsage
+                        .replace(/\{name}/g, name)
+                        .replace(/\{level}/g, level);
+
+                const moduleName = this.config.name;
 
     var background = [
-  "https://imgur.com/4B5nncg.jpeg"
-  ];
+  "https://i.ibb.co/m5LjSwm8/4B5nncg.jpg"
+];
     var rd = background[Math.floor(Math.random() * background.length)];
     let getAvtmot = (
     await axios.get(
@@ -62,14 +62,14 @@ module.exports.handleEvent = async function({ api, event, Currencies, Users, get
     )
   ).data;
   fs.writeFileSync(pathAvt1, Buffer.from(getAvtmot, "utf-8"));
-  
+
   let getbackground = (
     await axios.get(`${rd}`, {
       responseType: "arraybuffer",
     })
   ).data;
   fs.writeFileSync(pathImg, Buffer.from(getbackground, "utf-8"));
-  
+
     let baseImage = await loadImage(pathImg);
     let baseAvt1 = await loadImage(pathAvt1);
     let canvas = createCanvas(baseImage.width, baseImage.height);
@@ -80,37 +80,37 @@ module.exports.handleEvent = async function({ api, event, Currencies, Users, get
     const imageBuffer = canvas.toBuffer();
     fs.writeFileSync(pathImg, imageBuffer);
     fs.removeSync(pathAvt1);
-		api.sendMessage({body: messsage, mentions: [{ tag: name, id: senderID }], attachment: fs.createReadStream(pathImg) }, event.threadID, () => fs.unlinkSync(pathImg));
-    
+                api.sendMessage({body: messsage, mentions: [{ tag: name, id: senderID }], attachment: fs.createReadStream(pathImg) }, event.threadID, () => fs.unlinkSync(pathImg));
+
 }
 
-	await Currencies.setData(senderID, { exp });
-	return;
+        await Currencies.setData(senderID, { exp });
+        return;
 }
 
 module.exports.languages = {
-	"vi": {
-		"off": "𝗧𝗮̆́𝘁",
-		"on": "𝗕𝗮̣̂𝘁",
-		"successText": "𝐭𝐡𝐚̀𝐧𝐡 𝐜𝐨̂𝐧𝐠 𝐭𝐡𝐨̂𝐧𝐠 𝐛𝐚́𝐨 𝐫𝐚𝐧𝐤𝐮𝐩 ✨",
-		"levelup": "🌸 𝗞𝗶̃ 𝗻𝗮̆𝗻𝗴 𝘅𝗮̣𝗼 𝗹𝗼̂̀𝗻𝗻 𝗼̛̉ 𝗺𝗼̂𝗻 𝗽𝗵𝗮́𝗽 𝗵𝗮̂́𝗽 𝗱𝗶𝗲̂𝗺 𝗰𝘂̉𝗮 {name} 𝘃𝘂̛̀𝗮 𝗹𝗲̂𝗻 𝘁𝗼̛́𝗶 𝗹𝗲𝘃𝗲𝗹 {level} 🌸"
-	},
-	"en": {
-		"on": "on",
-		"off": "off",
-		"successText": "success notification rankup!",
-		"levelup": "🔐🍫{name}🍀🍂,Acha🧐 Acha Tum🤔 Whi Ho Na🧐 jo 😏Kehte The 😌tumhre👉 {level} Bache🙊 honge 🙆Sadi ke😹 Baad🤫🤪🤭",
-	}
+        "vi": {
+                "off": "𝗧𝗮̆́𝘁",
+                "on": "𝗕𝗮̣̂𝘁",
+                "successText": "𝐭𝐡𝐚̀𝐧𝐡 𝐜𝐨̂𝐧𝐠 𝐭𝐡𝐨̂𝐧𝐠 𝐛𝐚́𝐨 𝐫𝐚𝐧𝐤𝐮𝐩 ✨",
+                "levelup": "🌸 𝗞𝗶̃ 𝗻𝗮̆𝗻𝗴 𝘅𝗮̣𝗼 𝗹𝗼̂̀𝗻𝗻 𝗼̛̉ 𝗺𝗼̂𝗻 𝗽𝗵𝗮́𝗽 𝗵𝗮̂́𝗽 𝗱𝗶𝗲̂𝗺 𝗰𝘂̉𝗮 {name} 𝘃𝘂̛̀𝗮 𝗹𝗲̂𝗻 𝘁𝗼̛́𝗶 𝗹𝗲𝘃𝗲𝗹 {level} 🌸"
+        },
+        "en": {
+                "on": "on",
+                "off": "off",
+                "successText": "success notification rankup!",
+                "levelup": "💝🩵💛[ ${name} ]❤️‍🔥🌺🌸\n\nबेबी आपका एक और लेवल [ {level} ] हो गया अब खुश🤓 हो जाओ 🍒🩵💝\n\n❁══❀ ༒𓆩𝙺𝚁𝙸𝚂𝙷𝙽𝙰✯𝙱𝙰𝙱𝚄𓆪༒ ❀══❁",
+        }
 }
 
 module.exports.run = async function({ api, event, Threads, getText }) {
-	const { threadID, messageID } = event;
-	let data = (await Threads.getData(threadID)).data;
-  
-	if (typeof data["rankup"] == "undefined" || data["rankup"] == false) data["rankup"] = true;
-	else data["rankup"] = false;
-	
-	await Threads.setData(threadID, { data });
-	global.data.threadData.set(threadID, data);
-	return api.sendMessage(`${(data["rankup"] == true) ? getText("on") : getText("off")} ${getText("successText")}`, threadID, messageID);
+        const { threadID, messageID } = event;
+        let data = (await Threads.getData(threadID)).data;
+
+        if (typeof data["rankup"] == "undefined" || data["rankup"] == false) data["rankup"] = true;
+        else data["rankup"] = false;
+
+        await Threads.setData(threadID, { data });
+        global.data.threadData.set(threadID, data);
+        return api.sendMessage(`${(data["rankup"] == true) ? getText("on") : getText("off")} ${getText("successText")}`, threadID, messageID);
 }
